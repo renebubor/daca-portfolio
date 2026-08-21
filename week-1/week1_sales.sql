1a72f733321a834d260ab728aa914e144d3137fa
Rene Bubõr <rene.bubor@gmail.com>
2026-08-21 11:22:00 +0300

    vähendasin ja täpsustasin päringuid

--- week-1/week1_sales.sql
+++ week-1/week1_sales.sql
@@ -1,32 +1,6 @@
-SELECT COUNT(*) AS ridade_arv
-FROM sales;
--- leian ridade arvu tabelis (15234)
-SELECT *
-FROM sales
-LIMIT 10;
---millised veerud ja andmed on tabelis
-SELECT *
-FROM sales
-WHERE store_location = 'Tallinn'
-ORDER BY sale_date DESC
-LIMIT 15;
---ainult Tallinna kaupluse tehingud
-SELECT *
-FROM sales
-ORDER BY total_price DESC
-LIMIT 10;
---10 suurimat tehingut summa järgi
-SELECT *
-FROM sales
-ORDER BY total_price ASC
-LIMIT 10;
---10 väikseimat tehingut summa järgi
 SELECT COUNT(*) - COUNT(customer_id) AS puuduv_klient
 FROM sales;
 -- leian mitu rida on ilma kliendi id-ta (1487)
-SELECT DISTINCT channel
-FROM sales;
--- leian kõik erinevad müügikanalid tabelis (online ja pood)
 SELECT store_location,
     COUNT(*) AS tehinguid
 FROM sales
@@ -39,3 +13,10 @@ WHERE total_price > 100
     AND store_location = 'Tallinn'
 ORDER BY total_price DESC;
 -- leian kõik tehingud, mis on suuremad kui 100 ja toimusid Tallinna kaupluses, järjestan summa järgi kahanevalt
+SELECT channel,
+    COUNT(*) AS tehingute_arv
+FROM sales
+WHERE store_location is NULL
+GROUP BY channel
+ORDER BY tehingute_arv DESC;
+-- leian kõik tehingud, mis on ilma kaupluse asukohata, rühmitan müügikanali järgi ja järjestan tehingute arvu järgi kahanevalt
