b45b5575a277e4557c484b82fa971c360f1cfe9f
Rene Bubõr <rene.bubor@gmail.com>
2026-08-21 09:45:43 +0300

    edasijõudnute osa lisatud

--- week-1/week1_sales.sql
+++ week-1/week1_sales.sql
@@ -24,3 +24,18 @@ LIMIT 10;
 SELECT COUNT(*) - COUNT(customer_id) AS puuduv_klient
 FROM sales;
 -- leian mitu rida on ilma kliendi id-ta (1487)
+SELECT DISTINCT channel
+FROM sales;
+-- leian kõik erinevad müügikanalid tabelis (online ja pood)
+SELECT store_location,
+    COUNT(*) AS tehinguid
+FROM sales
+GROUP BY store_location
+ORDER BY tehinguid DESC;
+-- leian kaupluse asukoha ja tehingute arvu, rühmitan asukoha järgi ja järjestan tehingute arvu järgi kahanevalt
+SELECT *
+FROM sales
+WHERE total_price > 100
+    AND store_location = 'Tallinn'
+ORDER BY total_price DESC;
+-- leian kõik tehingud, mis on suuremad kui 100 ja toimusid Tallinna kaupluses, järjestan summa järgi kahanevalt
