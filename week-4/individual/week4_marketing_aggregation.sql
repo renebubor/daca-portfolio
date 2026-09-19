9c5a534c88f7e7a8f8515a165aed9eb3ea3cc5e0
Rene Bubõr <rene.bubor@gmail.com>
2026-09-19 14:10:26 +0300

    W4 CTE valemi parandus

--- week-4/individual/week4_marketing_aggregation.sql
+++ week-4/individual/week4_marketing_aggregation.sql
@@ -14,93 +14,32 @@ FROM sales o
 GROUP BY w.source
 ORDER BY kogukäive DESC;
 --2. Kanali efektiivsus CTE-ga
-WITH turunduskanali_myyk AS (
-    -- CTE 1: kanali kogumüük 
-    SELECT w.source AS turunduskanal,
-        COUNT(DISTINCT o.sale_id) AS tellimusi,
-        SUM(o.total_price) AS kogukäive
-    FROM sales o
-        JOIN customers c ON o.customer_id = c.customer_id
-        LEFT JOIN web_logs w ON c.customer_id = w.customer_id
-    GROUP BY w.source
-    HAVING COUNT(DISTINCT o.sale_id) > 500 --rohkem kui 500 tellimust
-),
-unikaalsed_kliendid AS (
-    -- CTE 2: kanali unikaalsete klientide arv
-    SELECT w.source AS turunduskanal,
-        COUNT(DISTINCT c.customer_id) AS kliente
-    FROM customers c
-        LEFT JOIN web_logs w ON c.customer_id = w.customer_id
-    GROUP BY w.source
+WITH kliendi_kanal AS (
+    SELECT customer_id,
+        source AS turunduskanal
+    FROM (
+            SELECT customer_id,
+                source,
+                ROW_NUMBER() OVER (
+                    PARTITION BY customer_id
+                    ORDER BY visit_date
+                ) AS rn
+            FROM web_logs
+            WHERE source IS NOT NULL
+        ) x
+    WHERE rn = 1
 )
-SELECT tm.turunduskanal,
-    uk.kliente,
-    tm.tellimusi,
-    tm.kogukäive,
-    ROUND(tm.kogukäive / NULLIF(uk.kliente, 0), 2) AS müük_per_klient
-FROM turunduskanali_myyk tm
-    JOIN unikaalsed_kliendid uk ON tm.turunduskanal = uk.turunduskanal
-ORDER BY müük_per_klient DESC;
---3.Kampaaniate kuised trendid
-SELECT w.source AS turunduskanal,
-    DATE_TRUNC('month', o.sale_date) AS kuu,
-    SUM(o.total_price) AS kogukäive,
-    COUNT(DISTINCT o.customer_id) AS unikaalseid_kliente,
-    COUNT(DISTINCT o.sale_id) AS tellimusi
-FROM sales o
-    JOIN customers c ON o.customer_id = c.customer_id
-    LEFT JOIN web_logs w ON c.customer_id = w.customer_id
-GROUP BY w.source,
-    DATE_TRUNC('month', o.sale_date)
-HAVING COUNT(DISTINCT o.sale_id) > 20 --vähem kui 20 tellimust välistatud
-ORDER BY kuu,
-    kogukäive DESC;
---4.Esitlus juhatusele:
-/* Meie kõige efektiivsem kanal on kogukäibe järgi google_organic, mille keskmine tellimus on 286 €.*/
----
-WITH turunduskanali_myyk AS (
-    -- CTE 1: kanali kogumüük 
-    SELECT w.source AS turunduskanal,
-        DATE_TRUNC('month', o.sale_date) AS kuu,
-        COUNT(DISTINCT o.sale_id) AS tellimusi,
-        SUM(o.total_price) AS kogukäive
-    FROM sales o
-        JOIN customers c ON o.customer_id = c.customer_id
-        LEFT JOIN web_logs w ON c.customer_id = w.customer_id
-    GROUP BY w.source,
-        DATE_TRUNC('month', o.sale_date)
-    HAVING COUNT(DISTINCT o.sale_id) > 10 --rohkem kui 10 tellimust
-),
-myyk_eelmise_kuuga AS (
-    -- CTE 2: lisan eelmise kuu käibe
-    SELECT turunduskanal,
-        kuu,
-        tellimusi,
-        kogukäive,
-        LAG(kogukäive) OVER (
-            PARTITION BY turunduskanal
-            ORDER BY kuu
-        ) AS eelmise_kuu_käive
-    FROM turunduskanali_myyk
-),
-unikaalsed_kliendid AS (
-    -- CTE 3: kanali unikaalsete klientide arv
-    SELECT w.source AS turunduskanal,
-        COUNT(DISTINCT c.customer_id) AS kliente
-    FROM customers c
-        LEFT JOIN web_logs w ON c.customer_id = w.customer_id
-    GROUP BY w.source
-)
-SELECT tm.turunduskanal,
-    MAX(uk.kliente) AS kliente,
-    SUM(tm.tellimusi) AS tellimusi,
-    SUM(tm.kogukäive) AS käive,
-    SUM(tm.eelmise_kuu_käive) AS eelmise_kuu_käive,
+SELECT kk.turunduskanal,
+    COUNT(DISTINCT s.customer_id) AS kliente,
+    COUNT(s.sale_id) AS tellimusi,
+    SUM(s.total_price) AS kogukäive,
     ROUND(
-        SUM(tm.kogukäive) / NULLIF(MAX(uk.kliente), 0),
+        SUM(s.total_price) / NULLIF(COUNT(DISTINCT s.customer_id), 0),
         2
     ) AS müük_per_klient
-FROM myyk_eelmise_kuuga tm
-    JOIN unikaalsed_kliendid uk ON tm.turunduskanal = uk.turunduskanal
-GROUP BY tm.turunduskanal
-ORDER BY müük_per_klient DESC;
+FROM sales s
+    LEFT JOIN kliendi_kanal kk ON s.customer_id = kk.customer_id
+GROUP BY kk.turunduskanal
+ORDER BY kogukäive DESC;
+--4.Esitlus juhatusele:
+/* Meie kõige efektiivsem kanal on kogukäibe järgi google_organic, mille keskmine tellimus on 1024,7 €.*/
